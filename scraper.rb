da2c5c9e1004039047272c85e31c10d4966b6907
Luke Bacon <luke@equivalentideas.com>
2015-04-15 07:23:42 +1000

    initial scraper to collect data from hard coded page url

--- scraper.rb
+++ scraper.rb
@@ -1,17 +1,60 @@
 # This is a template for a Ruby scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
+require 'scraperwiki'
+require 'mechanize'
+# require 'redcarpet'
+
+# def markdown(html)
+#   Redcarpet.new(html).html_safe
+# end
+
+def trim_item_body(post)
+  # TODO: test this works across posts
+  post.children[0...3].remove
+  post.search('p.backlink').remove
+
+  # TODO: convert to markdown
+  return post.to_html
+end
+
+def save_news_item(page, domain)
+  url = page.uri.to_s
+
+  id = url.to_s[/id=(\d+)/, 1]
+
+  post_div = page.at('div.content + div.content')
+
+  pub_date = post_div.search(:h2)[0] ? post_div.search(:h2)[0].text : nil
+
+  item_body_html = trim_item_body(post_div)
+
+  # Build Array of attached files
+  attached_files = post_div.at('.filelist').search(:a).map { |a| domain + a.attr(:href) }
+
+  record = {
+    id: id,
+    url: url,
+    item_title: page.search('h1.pagetitle').text,
+    pub_date: pub_date,
+    attached_files: attached_files,
+    item_body: item_body_html
+  }
+
+  p record
+
+  # ScraperWiki.save_sqlite([:id], record)
+end
+
+agent = Mechanize.new
+
+base_url = 'http://www.santos.com'
+page_address = '/Archive/NewsDetail.aspx?p=121&id=1485'
+
+page = agent.get(base_url + page_address)
+
+save_news_item(page, base_url)
+
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
 #
